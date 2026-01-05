89eabdb256e633154cc0edf9e087c5a192a6b808
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2026-01-05 23:01:05 +0100

    pretty navbar

--- src/app/features/user/pages/register/register.component.ts
+++ src/app/features/user/pages/register/register.component.ts
@@ -30,7 +30,7 @@ export class RegisterComponent {
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
     address: ['', Validators.required],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
     confirmPassword: ['', Validators.required],
   });
 
